032b066f168bd2feeb0e97f2f44c78a296b57efb
nizam <nizamthonas123@gmail.com>
2024-07-01 18:06:10 +0530

    python file -- multiplication table

---
+++ python.py
@@ -0,0 +1,8 @@
+# Multiplication Table in Python
+print("multiplication table of 5")
+num = 5
+
+print(f"The multiplication table of {num}:")
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
+
